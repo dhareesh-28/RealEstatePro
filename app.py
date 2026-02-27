f26cc33af60c8b85f2f876094db6e73420bbcc3b
dhareesh-28 <dhareeshb2005@gmail.com>
2026-02-27 06:53:41 +0530

    Update app.run to use host=0.0.0.0 and port=8000

--- app.py
+++ app.py
@@ -396,4 +396,4 @@ def send_conversation_message(conversation_id):
 
 if __name__ == '__main__':
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
